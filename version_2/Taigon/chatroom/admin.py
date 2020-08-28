31b86763c46ea01df66791078894cb24e3ffe9e3
Cehng Yi Lin <cheng.yi.lin.217@gmail.com>
2020-08-28 15:22:57 +0800

    remove unused folder

--- version_2/Taigon/chatroom/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import ChatRooom, RoomMember, Message, RooomCategory
-
-# Register your models here.
-admin.site.register(ChatRooom)
-admin.site.register(RoomMember)
-admin.site.register(Message)
-admin.site.register(RooomCategory)
--- version_2/Taigon/frontend/src/actions/createroom.js
+++
@@ -1,7 +0,0 @@
-export const TOGGLE_CREATEROOM = 'TOGGLE_CREATEROOM';
-
-export const toggleCreateRoom = () => {
-    return {
-        type: TOGGLE_CREATEROOM
-    }
-}
